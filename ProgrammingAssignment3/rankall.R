0147718d89fb643e4d03e383a76e9a1b4c1d6236
eamd <eamd1030@users.noreply.github.com>
2015-11-29 01:37:11 -0600

    First pass of code. Rankall implemented.

--- ProgrammingAssignment3/rankall.R
+++ ProgrammingAssignment3/rankall.R
@@ -1,4 +1,4 @@
-rankall <- function(outcome = NULL, rank = '1', data = 'ProgAssignment3-data/outcome-of-care-measures.csv') {
+rankall <- function(outcome = 'heart attack', rank = '1', data = 'ProgAssignment3-data/outcome-of-care-measures.csv') {
   
   rankall <- NULL
 
@@ -30,12 +30,35 @@ rankall <- function(outcome = NULL, rank = '1', data = 'ProgAssignment3-data/out
     getRank
   }
   
+  getRankedHospital <- function(state, rank, dataset) {
+    
+    getRankedHospital <- NA
+
+    dfs <- subset(dataset, dataset$State == state, select=c('Hospital.Name', 'State', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure', 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'))
+    names(dfs) <- c('Hospital', 'State','Heart Attack', 'Heart Failure', 'Pneumonia')
+    rowCt <- nrow(dfs)
+    
+    if(rowCt > 0) {
+      valid_rows <- subset(dfs, dfs[bestOutcome(outcome)] != 'Not Available', select=c('Hospital', bestOutcome(outcome)))
+      rowCt <- nrow(valid_rows)
+      if(rowCt > 0) {
+        svr <- valid_rows[order(as.numeric(valid_rows[,2]),valid_rows[,1]), ]
+    
+        if(getRank(rank,rowCt) <= rowCt) {
+          getRankedHospital <- svr[getRank(rank,rowCt),1]
+        }
+      }
+    }    
+    
+    getRankedHospital
+  }
+  
   ##
-  ## Use the state data set to build a list of valid USPS
+  ## Use the hospital data file to build a list of valid USPS
   ## state abbreviations
   ##
-  data(state) # load US states dataset
-  stateabb <- state.abb
+  hosp <- read.csv('ProgAssignment3-data/hospital-data.csv', colClasses = 'character')
+  stateabb <- (sort(unique(hosp$State), decreasing = FALSE))
   
   outc <- c('heart attack', 'heart failure', 'pneumonia')
   if(!outcome %in% outc){
@@ -45,9 +68,13 @@ rankall <- function(outcome = NULL, rank = '1', data = 'ProgAssignment3-data/out
   ##
   ## Read in the data
   df <- read.csv(data, colClasses = 'character')
+
+  resdf <- sapply(stateabb, getRankedHospital, rank, df)
+
+  resdf <- as.data.frame(cbind(resdf, stateabb))
+  names(resdf) <- c('hospital', 'state')
+
+  rankall <-resdf
   
-  x <- sapply(stateabb,function(x){print(paste('State: ', x))})
-  
-  print(class(x))
   rankall
 }
