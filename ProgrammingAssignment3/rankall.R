8d899f458ab93fc8ee4151aa867fe466a0046c82
eamd <eamd1030@users.noreply.github.com>
2015-11-29 03:13:12 -0600

    Cleaned up code and provided documentation.

--- ProgrammingAssignment3/rankall.R
+++ ProgrammingAssignment3/rankall.R
@@ -1,51 +1,86 @@
-rankall <- function(outcome = 'heart attack', rank = '1', data = 'ProgAssignment3-data/outcome-of-care-measures.csv') {
+rankall <- function(outcome = NULL, rank = 1, datapath = paste(getwd(),'/ProgAssignment3-data/', sep=''), data = 'outcome-of-care-measures.csv') {
   
   rankall <- NULL
-
-  ##
-  ## bestOutcome takes the passed outcome value and
-  ## reformats it to match the column heading for the subsetted dataframes.
-  ##
+  datafile <- paste(datapath, data, sep='')
+  hospfile <- paste(datapath, 'hospital-data.csv', sep='' )
   
-  bestOutcome <- function(outcome){
-    if(outcome == 'pneumonia'){
-      offset = 'Pneumonia'
-    } else if (outcome == 'heart failure') {
-      offset = 'Heart Failure'
-    } else {
-      offset = 'Heart Attack'
-    }
-    offset
-  }
-  
-  getRank <- function(theRank, dsSize){
+  ##
+  ## Function getRank returns a numeric value for the rank entered.
+  ## If the term, 'best' is entered, the rank will be set to 1.
+  ##
+  ## If the term, 'worst' is entered, the rank will be set to the number of
+  ## of rows in the supplied dataset (dsSize).
+  ##
+  ## If a number is supplied (in character format) convert it to a numeric vector.
+  ## This last check is just in case somebody enters a character value for the 
+  ## rank.
+  ##
+  validRank <- function(theRank){
+    validRank <- FALSE
     
-    if(toupper(theRank) == 'BEST'){
-      getRank <- 1
-    } else if(toupper(theRank)=='WORST'){
-      getRank <- dsSize
+    if(tolower(theRank) %in% c('best', 'worst')){
+      validRank <- TRUE
     } else {
-      getRank <- as.numeric(theRank)
+      if(suppressWarnings(is.numeric(as.numeric(theRank)))) {
+        validRank <- TRUE
+      }
     }
-    getRank
+  validRank
   }
   
-  getRankedHospital <- function(state, rank, dataset) {
+  calcRank <- function(rank, ds) {
+    if(tolower(rank) == 'best') {
+      calcRank <- 1
+    } else if(tolower(rank) == 'worst') {
+      calcRank <- nrow(ds)
+    } else calcRank <- as.numeric(rank)
+  }
+  
+  ##
+  ## Find the ranked hospital for the given state.
+  ##
+  
+  getRankedHospital <- function(state, rank) {
     
     getRankedHospital <- NA
 
-    dfs <- subset(dataset, dataset$State == state, select=c('Hospital.Name', 'State', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure', 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'))
-    names(dfs) <- c('Hospital', 'State','Heart Attack', 'Heart Failure', 'Pneumonia')
+    ##
+    ## Extract the rows for  the specified state.
+    ##
+    ## Additionally, specify the columns from the dataset that will be used
+    ##
+    dfs <- subset(df, df$State == state, select=c('Hospital.Name', 'State', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack', 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure', 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia'))
+
+    ##
+    ## Relabel the column names to be easier to reference
+    ##
+    names(dfs) <- c('hospital', 'state','heart attack', 'heart failure', 'pneumonia')
+    
+    ## Calculate the number of hospitals retrieved for the specified state.
     rowCt <- nrow(dfs)
     
+    ##
+    ## Remove the rows that have a value 'Not Available' for the specified outcome
+    ## and revise the number of hospitals retrieved (row count)
+    ##
     if(rowCt > 0) {
-      valid_rows <- subset(dfs, dfs[bestOutcome(outcome)] != 'Not Available', select=c('Hospital', bestOutcome(outcome)))
+      valid_rows <- subset(dfs, dfs[outcome] != 'Not Available', select=c('hospital', outcome))
       rowCt <- nrow(valid_rows)
       if(rowCt > 0) {
+        
+        ##
+        ## Sort rows by outcome, then by hospital within outcome. This is
+        ## for when multiple hospitals have the same outcome rate. The hospitals are
+        ## sorted by name in ascending alphabetical order.
+        ##
         svr <- valid_rows[order(as.numeric(valid_rows[,2]),valid_rows[,1]), ]
     
-        if(getRank(rank,rowCt) <= rowCt) {
-          getRankedHospital <- svr[getRank(rank,rowCt),1]
+        ##
+        ## Get the ranked hospital for the state specified.
+        ##
+        theRank <- calcRank(rank, svr)
+        if(theRank <= rowCt) { # Note: if theRank is greater than rowCt, the function will return NA (defined earlier)
+          getRankedHospital <- svr[theRank,1]
         }
       }
     }    
@@ -54,27 +89,72 @@ rankall <- function(outcome = 'heart attack', rank = '1', data = 'ProgAssignment
   }
   
   ##
-  ## Use the hospital data file to build a list of valid USPS
-  ## state abbreviations
+  ## Verify that the outcome submitted, is valid
+  ## heart attack, heart failure and pneumonia are the only accepted outcomes
   ##
-  hosp <- read.csv('ProgAssignment3-data/hospital-data.csv', colClasses = 'character')
-  stateabb <- (sort(unique(hosp$State), decreasing = FALSE))
   
+  if(is.null(outcome)) {
+    stop('invalid outcome')
+  }
   outc <- c('heart attack', 'heart failure', 'pneumonia')
   if(!outcome %in% outc){
     stop('invalid outcome')
   }
-  
+  outcome <- tolower(outcome)
+
+  ##
+  ## Use the hospital data file to build a list of valid USPS
+  ## state abbreviations
+  ##
+  if(file.exists(hospfile)){
+    hosp <- read.csv(hospfile, colClasses = 'character')
+    stateabb <- (sort(unique(hosp$State), decreasing = FALSE))
+  } else {
+    stop(paste(hospfile), 'is an invalid path for the hospital data file.')
+  }
   ##
   ## Read in the data
-  df <- read.csv(data, colClasses = 'character')
+  ##
+  
+  if(file.exists(datafile)) {
+    df <- read.csv(datafile, colClasses = 'character')
+  } else {
+    stop(paste(datafile,'is an invalid path for the outcome data file.'))
+  }
+  
+  ##
+  ## Check for a valid rank entered
+  ##
+  if(!validRank(rank)) {
+    stop('invalid rank')
+  }
+  
+  ##
+  ## For each state in the stateabb list, find the hospital for  the rank provided.
+  ## The result will be returned as a character vector, one element for each state.
+  ##
+  resdf <- sapply(stateabb, getRankedHospital, rank)
 
-  resdf <- sapply(stateabb, getRankedHospital, rank, df)
-
+  ##
+  ## The number of elements in resdf should be the same as in the vector, stateabb.
+  ## The the two vectors can be column bound into a dataframe.
+  ## The resulting data frame is stored in resdf (why create another variable?).
+  ##
   resdf <- as.data.frame(cbind(resdf, stateabb))
+
+  ## Since I was changing column names earlier, I had to set them to the names (formatted as well) 
+  ## specified in the assignment documentation.
   names(resdf) <- c('hospital', 'state')
 
+  ##
+  ## Return the data frame to the function name in case, this data frame
+  ## will be used in other code.
+  ##
   rankall <-resdf
   
+  ##
+  ## So we can see the results of the function call,
+  ## When called from the command line.
+  ##
   rankall
 }
